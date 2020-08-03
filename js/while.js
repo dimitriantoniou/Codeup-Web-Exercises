6010dd739b27e02d2c3a39559c574484a61ba3e0
dimitriantoniou <dma2012@gmail.com>
2020-08-03 09:08:45 -0500

    while loop exercises (still in progress)
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- js/while.js
+++ js/while.js
@@ -9,8 +9,33 @@ while (i<=65536){
     i*=2;
 }
 
+//do while loop exercise
 
 // This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
+//do this thing; check if it's trye; repeat if yes, end if not
+/*
+var i = 10;
+
+do {
+    console.log('while loop iteration #' + i);
+    i++;
+} while (i < 10);
+ */
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log('We started with ' + allCones + ' cones.');
+var conesRemaining = allCones;
+
+do {
+    var boughtEachTime = Math.floor(Math.random() * 5) + 1;
+    console.log('Number of cones left: ' + conesRemaining);
+    if (conesRemaining >= boughtEachTime) {
+        console.log(boughtEachTime + ' cones sold...');
+        conesRemaining = conesRemaining - boughtEachTime;
+    } else if (boughtEachTime > conesRemaining) {
+        console.log('I cannot sell you ' + boughtEachTime + ' I only have ' + conesRemaining + ' left.')
+    }
+
+} while (conesRemaining = 0);
+console.log('Yay. sold them all.');
