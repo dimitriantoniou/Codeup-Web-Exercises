95477c6b407fe305017ba6c02ebe0e24866ae220
dimitriantoniou <dma2012@gmail.com>
2020-08-04 09:24:45 -0500

    completed exercises
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- js/iterating.js
+++ js/iterating.js
@@ -1,5 +1,5 @@
 (function(){
-
+//completed
     "use strict";
 
     /**
@@ -30,9 +30,10 @@
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    for (var i =0;i<5;i++){
+    for (var i =0;i<4;i++){
         console.log("Say hello to " +names[i]+"!")
     }
+    //line 34 is console logging an udnefined result
 
     /**
      * TODO:
@@ -41,6 +42,7 @@
     names.forEach(function(name){
         console.log("Say hello to " + name + '!');
     })
+
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -54,47 +56,19 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-/*
-function first(arr) {
-        return arr[0];
+
+    function first (arr) {
+        console.log(arr[0]);
+        }
+    function second (arr){
+        console.log(arr[1]);
+    }
+    function last (arr) {
+        console.log(arr[arr.length-1]);
     }
 
-    var numbers = [1, 2, 3, 4, 5];
-    var result = first(numbers);
-    console.log('first test: ' , result === 1);
-
-    console.log(first([1, 2, 3, 4, 5]));
-    console.log(first(["purple", 3, 2.5]));
-
-    function second(arr) {
-        return arr[1];
-    }
-
-    result = second(numbers);
-    console.log('second test: ' , result === 2);
-
-    console.log(second([1, 2, 3, 4, 5]));
-    console.log(second(["purple", 3, 2.5]));
-
-    function last(arr) {
-        return arr[arr.length - 1];
-    }
-
-    result = last(numbers);
-    console.log('last test: ' , result === 5);
-
-    console.log(last([1, 2, 3, 4, 5]));
-    console.log(last(["purple", 3, 2.5]));
- */
-    //first
-    function firstNumber(array) {
-        return (array[0]);
-    }
-    var numbers=[1,2,3,4,5];
-    var result = firstNumber(numbers);
-    console.log(first(numbers));
-//second
-
-//third
+    first(names);
+    second(names);
+    last(names);
 
 })();
