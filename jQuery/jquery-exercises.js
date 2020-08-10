ad8924f82f8eff3bf3fe01152411a74f9633b04b
dimitriantoniou <dma2012@gmail.com>
2020-08-10 17:04:42 -0500

    completex selectors exercises
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- jQuery/jquery-exercises.js
+++ jQuery/jquery-exercises.js
@@ -23,12 +23,25 @@ $(document).ready(function() {
 //Update your code so that at least 3 different elements have the same class named codeup.
 
 //Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
-$('codeup').css('border', 'red');
+$('.codeup').css('border', '1 px solid #F00');
 
 //Remove the class from one of the elements. Refresh and test that the border has been removed.
 
 //Give another element an id of codeup. Does this element get a border now?
 
+//Remove your custom jQuery code from previous exercises.
+
+//Using jQuery, set the font-size of all li elements to 20px.
+$('li').css('font-size','20px');
+
+// Craft selectors that highlight all the h1, p, and li elements.
+
+//Create a jQuery statement to alert the contents of your h1 element(s).
+var headers = $('h1').html();
+alert(headers);
+
+//Combine your selectors that highlight all the h1, p, and li elements.
+$('.codeup,li,p');
 
 //lesson notes
 
