a98f984b1e15ec1fdb74d8f9f7340748ec89bad0
dimitriantoniou <dma2012@gmail.com>
2020-08-10 12:36:07 -0500

    jquery selectors lesson notes
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- jQuery/jquery-exercises.js
+++ jQuery/jquery-exercises.js
@@ -3,3 +3,17 @@
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+
+var contents = $('#codeup').html();
+alert(contents);
+/*
+
+$('.important').css('background-color', 'yellow');
+
+$('p').css('font-size', '14px');
+*/
+
+$('.important, p').css('background-color', '#FF0');
+
+$('*').css('border', '1px solid #F00');
