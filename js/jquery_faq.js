e4ae2e4669a33a184f36d185043d2c32967b160e
dimitriantoniou <dma2012@gmail.com>
2020-08-18 20:02:46 -0500

    completed jquery and attribute css exercise
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -7,8 +7,23 @@
 //list li in each ul
 //yellow background
 
+/*
 $(document).ready(function() {
     $('#list-style-item').click(function() {
         $('li').last().css('backgroundColor','#FF0');
     });
+});
+*/
+
+
+//add invisible class to all dd elements
+$(document).ready(function() {
+    $("dd").addClass("invisible");
+});
+
+
+$(document).ready(function() {
+    $('#dd-hide').click(function (event) {
+        $('dd').toggleClass('invisible');
+    })
 });
