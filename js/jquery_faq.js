a485cc762385de5ac2fde1e308dd59c5682a3490
dimitriantoniou <dma2012@gmail.com>
2020-08-18 20:39:06 -0500

    completed jquery effects exercise
    Signed-off-by: dimitriantoniou <dma2012@gmail.com>

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -12,6 +12,7 @@
 //add invisible class to all dd elements
 $(document).ready(function() {
     $("dd").addClass("invisible");
+    $('#sign-up').hide();
 });
 
 //toggle visibility of dd elements
@@ -32,12 +33,38 @@ $('#list-style-time').click(function() {
     });
 });
 
+/*
 //bold list on h3 click
 $('h3').click(function() {
     $(this).next().children().css('font-weight','bold');
 });
+ */
 
 //When any list item is clicked, first li of the parent ul should have a font color of blue.
 $('li').click(function(){
    $(this).parent().children().first().css('color','blue')
 });
+
+//When the 'x' is clicked, hide the entire div element.
+$('#x').click(function(){
+    $(this).hide();
+});
+
+/*
+The uls next to each h3 that contain facts about each park should start out hidden,
+ and when the corresponding h3 is clicked, the display of the ul should be toggled
+ with a sliding effect.
+*/
+$('h3').click(function(){
+    $(this).next().slideToggle(2500);
+})
+
+//After the user has been on the page for 8 seconds,
+// an h2 should fade in that asks the user to register for the site.
+
+
+setTimeout( function() {
+    $('#sign-up').fadeIn();
+    $('main').css('opacity', '0.1');
+    $('#sign-up').css('opacity', '1');
+}, 8000);
